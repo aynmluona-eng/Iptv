6edc38d79bc7430023af5cabd1c86c1a2aeae18f
aynmluona-eng <aynmluona@gmail.com>
2026-05-03 20:42:08 +0200

    setup capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iptv.app',
+  appName: 'Iptv',
+  webDir: 'dist'
+};
+
+export default config;
